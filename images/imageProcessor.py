6c076d1a3a3bfc033b3f1146489d0fd3d027258e
Yaroslav <122485322+Yaroslav1403@users.noreply.github.com>
2024-12-20 14:25:28 +0300

    Update imageProcessor.py

--- images/imageProcessor.py
+++ images/imageProcessor.py
@@ -1,16 +1,15 @@
-from PIL import ImageFilter, ImageDraw
+from PIL import ImageFilter, ImageDraw  #Импортируем модули ImageFilter и ImageDraw из библиотеки PIL
 
-class ImageProcessor:
-    def __init__(self, img):
-        self.img = img  
+class ImageProcessor:  #Определяем класс ImageProcessor
+    def __init__(self, img):  #Метод инициализации
+        self.img = img  #Инициализируем переменную для хранения изображения
 
-    def contour(self, img):
-        self.img = self.img.filter(ImageFilter.CONTOUR) 
-        return self.img  
+    def contour(self, img):  #Метод для применения контура к изображению
+        self.img = self.img.filter(ImageFilter.CONTOUR)  #Применяем фильтр контура к изображению
+        return self.img  #Возвращаем изображение с примененным контуром
 
-
-    def text_var(self):
-        size = self.img.size
-        draw = ImageDraw.Draw(self.img)
-        draw.text((size[0]/2,size[1]/2), 'option 3 ', fill = ('#1C0606'))
-        return self.img
+    def text_var(self):  #Метод для добавления текста к изображению
+        size = self.img.size  #Получаем размер изображения
+        draw = ImageDraw.Draw(self.img)  #Создаем объект для рисования на изображении
+        draw.text((size[0] / 2, size[1] / 2), 'option 3', fill=('#1C0606'))  #Добавляем текст в центр изображения
+        return self.img  #Возвращаем изображение с добавленным текстом
