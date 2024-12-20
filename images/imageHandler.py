225c050770d9709adcc46e1d3adfdc1fcf5d23b0
Yaroslav <122485322+Yaroslav1403@users.noreply.github.com>
2024-12-20 14:18:55 +0300

    Update imageHandler.py

--- images/imageHandler.py
+++ images/imageHandler.py
@@ -1,34 +1,28 @@
-from PIL import Image
-from .imageProcessor import *
-import os
+from PIL import Image  #Импортируем модуль Image из библиотеки PIL для обработки изображений
+from .imageProcessor import *  #Импортируем все из локального модуля imageProcessor
+import os  #Импортируем модуль os для работы с файловой системой
 
 
-class ImageHandler():
-    def __init__(self):
-        self.img = None  
-        self.file_path = None  
+class ImageHandler():  #Определяем класс ImageHandler
+    def __init__(self):  #Метод инициализации
+        self.img = None  #Инициализируем переменную для хранения изображения
+        self.file_path = None  #Инициализируем переменную для хранения пути к файлу
 
-    def load_image(self,file_path):
-        self.img = Image.open(self.file_path)
+    def load_image(self, file_path):  #Метод для загрузки изображения
+        self.img = Image.open(file_path)  #Открываем изображение по заданному пути и сохраняем в переменную img
         
-    def re_size(self):
-        max_size = (200,200) 
-        self.img.thumbnail(max_size) 
+    def re_size(self):  #Метод для изменения размера изображения
+        max_size = (200, 200)  #Устанавливаем максимальный размер
+        self.img.thumbnail(max_size)  #Изменяем размер изображения до максимального размера
 
-    def save_to_file(self, file_path, name):
-        self.img.save(self.file_path)  
-        os.rename(self.file_path, name)
+    def save_to_file(self, file_path, name):  #Метод для сохранения изображения в файл
+        self.img.save(file_path)  #Сохраняем изображение по заданному пути
+        os.rename(file_path, name)  #Переименовываем файл в заданное имя
     
-    def countor_load(self):
-        filter = ImageProcessor(self.img)
-        self.img = filter.contour(self.img)
+    def countor_load(self):  #Метод для применения контура к изображению
+        filter = ImageProcessor(self.img)  #Создаем объект класса ImageProcessor
+        self.img = filter.contour(self.img)  #Применяем метод контурирования и сохраняем результат в img
 
-    def text_var_load(self):
-        filter = ImageProcessor(self.img)
-        self.img = filter.text_var()
-    
-
-
-
-
-                      
+    def text_var_load(self):  #Метод для добавления текста к изображению
+        filter = ImageProcessor(self.img)  #Создаем объект класса ImageProcessor
+        self.img = filter.text_var()  #Применяем метод text_var и сохраняем результат в img
